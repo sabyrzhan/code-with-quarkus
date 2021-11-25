3c0e56cb3804e73309374ffd55966c789ee2a1df
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2021-11-25 23:47:14 +0200

    Initial commit

---
+++ src/main/java/kz/sabyrzhan/GreetingResource.java
@@ -0,0 +1,17 @@
+package kz.sabyrzhan;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        String str = "Hello in string";
+        return str;
+    }
+}
